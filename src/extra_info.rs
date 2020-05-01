5ee3565650ec2959dc5d2c5359210011ba9c5764
Logarithmus <logarithmus.ddev@gmail.com>
2020-05-01 06:06:43 +0300

    Fixed handling of case-agnostic MCU names (now tests pass again)

--- src/extra_info.rs
+++ src/extra_info.rs
@@ -13,93 +13,98 @@ pub struct Info {
 fn mmcu_from_mcu_name(mcu_name: &str) -> Architecture {
     use Architecture::*;
 
-    match mcu_name {
-        "AT90S1200" | "ATtiny11" | "ATtiny12" | "ATtiny15" | "ATtiny28" => Avr1,
+    match mcu_name.to_lowercase().as_ref() {
+        "at90s1200" | "attiny11" | "attiny12" | "attiny15" | "attiny28" => Avr1,
 
-        "AT90S2313" | "AT90S2323" | "AT90S2333" | "AT90S2343" | "ATtiny22" | "ATtiny26"
-        | "AT90S4414" | "AT90S4433" | "AT90S4434" | "AT90S8515" | "AT90S8534" | "AT90S8535" => Avr2,
+        "at90s2313" | "at90s2323" | "at90s2333" | "at90s2343" | "attiny22" | "attiny26"
+        | "at90s4414" | "at90s4433" | "at90s4434" | "at90s8515" | "at90s8534" | "at90s8535" => Avr2,
 
-        "ATA5272" | "ATA6616C" | "ATtiny13" | "ATtiny13A" | "ATtiny2313" | "ATtiny2313A"
-        | "ATtiny24" | "ATtiny24A" | "ATtiny4313" | "ATtiny44" | "ATtiny44A" | "ATtiny441"
-        | "ATtiny84" | "ATtiny84A" | "ATtiny25" | "ATtiny45" | "ATtiny85" | "ATtiny261"
-        | "ATtiny261A" | "ATtiny461" | "ATtiny461A" | "ATtiny861" | "ATtiny861A" | "ATtiny43U"
-        | "ATtiny87" | "ATtiny48" | "ATtiny88" | "ATtiny828" | "ATtiny841" | "AT86RF401" => Avr25,
+        "ata5272" | "ata6616c" | "attiny13" | "attiny13a" | "attiny2313" | "attiny2313a"
+        | "attiny24" | "attiny24a" | "attiny4313" | "attiny44" | "attiny44a" | "attiny441"
+        | "attiny84" | "attiny84a" | "attiny25" | "attiny45" | "attiny85" | "attiny261"
+        | "attiny261a" | "attiny461" | "attiny461a" | "attiny861" | "attiny861a" | "attiny43u"
+        | "attiny87" | "attiny48" | "attiny88" | "attiny828" | "attiny841" | "at86rf401" => Avr25,
 
-        "AT43USB355" | "AT76C711" => Avr3,
+        "at43usb355" | "at76c711" => Avr3,
 
-        "ATmega103" | "AT43USB320" => Avr31,
+        "atmega103" | "at43usb320" => Avr31,
 
-        "ATA5505" | "ATA6617C" | "ATA664251" | "AT90USB82" | "AT90USB162" | "ATmega8U2"
-        | "ATmega16U2" | "ATmega32U2" | "ATtiny167" | "ATtiny1634" => Avr35,
+        "ata5505" | "ata6617c" | "ata664251" | "at90usb82" | "at90usb162" | "atmega8u2"
+        | "atmega16u2" | "atmega32u2" | "attiny167" | "attiny1634" => Avr35,
 
-        "ATA6285" | "ATA6286" | "ATA6289" | "ATA6612C" | "ATmega8" | "ATmega8A" | "ATmega48"
-        | "ATmega48A" | "ATmega48P" | "ATmega48PA" | "ATmega48PB" | "ATmega88" | "ATmega88A"
-        | "ATmega88P" | "ATmega88PA" | "ATmega88PB" | "ATmega8515" | "ATmega8535"
-        | "ATmega8HVA" | "AT90PWM1" | "AT90PWM2" | "AT90PWM2B" | "AT90PWM3" | "AT90PWM3B"
-        | "AT90PWM81" => Avr4,
+        "ata6285" | "ata6286" | "ata6289" | "ata6612c" | "atmega8" | "atmega8a" | "atmega48"
+        | "atmega48a" | "atmega48p" | "atmega48pa" | "atmega48pb" | "atmega88" | "atmega88a"
+        | "atmega88p" | "atmega88pa" | "atmega88pb" | "atmega8515" | "atmega8535"
+        | "atmega8hva" | "at90pwm1" | "at90pwm2" | "at90pwm2b" | "at90pwm3" | "at90pwm3b"
+        | "at90pwm81" => Avr4,
 
-        "ATA5700M322" | "ATA5702M322" | "ATA5782" | "ATA5790" | "ATA5790N" | "ATA5791"
-        | "ATA5795" | "ATA5831" | "ATA6613C" | "ATA6614Q" | "ATA8210" | "ATA8215" | "ATA8510"
-        | "ATmega16" | "ATmega16A" | "ATmega161" | "ATmega162" | "ATmega163" | "ATmega164A"
-        | "ATmega164P" | "ATmega164PA" | "ATmega165" | "ATmega165A" | "ATmega165P"
-        | "ATmega165PA" | "ATmega168" | "ATmega168A" | "ATmega168P" | "ATmega168PA"
-        | "ATmega168PB" | "ATmega169" | "ATmega169A" | "ATmega169P" | "ATmega169PA"
-        | "ATmega16HVB" | "ATmega16HVBREVB" | "ATmega16M1" | "ATmega16U4" | "ATmega32A"
-        | "ATmega32" | "ATmega323" | "ATmega324A" | "ATmega324P" | "ATmega324PA" | "ATmega325"
-        | "ATmega325A" | "ATmega325P" | "ATmega325PA" | "ATmega3250" | "ATmega3250A"
-        | "ATmega3250P" | "ATmega3250PA" | "ATmega328" | "ATmega328P" | "ATmega328PB"
-        | "ATmega329" | "ATmega329A" | "ATmega329P" | "ATmega329PA" | "ATmega3290"
-        | "ATmega3290A" | "ATmega3290P" | "ATmega3290PA" | "ATmega32C1" | "ATmega32M1"
-        | "ATmega32U4" | "ATmega32U6" | "ATmega406" | "ATmega64" | "ATmega64A" | "ATmega640"
-        | "ATmega644" | "ATmega644A" | "ATmega644P" | "ATmega644PA" | "ATmega645"
-        | "ATmega645A" | "ATmega645P" | "ATmega6450" | "ATmega6450A" | "ATmega6450P"
-        | "ATmega649" | "ATmega649A" | "ATmega649P" | "ATmega6490" | "ATmega16HVA"
-        | "ATmega16HVA2" | "ATmega32HVB" | "ATmega6490A" | "ATmega6490P" | "ATmega64C1"
-        | "ATmega64M1" | "ATmega64HVE" | "ATmega64HVE2" | "ATmega64RFR2" | "ATmega644RFR2"
-        | "ATmega32HVBREVB" | "AT90CAN32" | "AT90CAN64" | "AT90PWM161" | "AT90PWM216"
-        | "AT90PWM316" | "AT90SCR100" | "AT90USB646" | "AT90USB647" | "AT94K" | "M3000" => Avr5,
+        "ata5700m322" | "ata5702m322" | "ata5782" | "ata5790" | "ata5790n" | "ata5791"
+        | "ata5795" | "ata5831" | "ata6613c" | "ata6614q" | "ata8210" | "ata8215" | "ata8510"
+        | "atmega16" | "atmega16a" | "atmega161" | "atmega162" | "atmega163" | "atmega164a"
+        | "atmega164p" | "atmega164pa" | "atmega165" | "atmega165a" | "atmega165p"
+        | "atmega165pa" | "atmega168" | "atmega168a" | "atmega168p" | "atmega168pa"
+        | "atmega168pb" | "atmega169" | "atmega169a" | "atmega169p" | "atmega169pa"
+        | "atmega16hvb" | "atmega16hvbrevb" | "atmega16m1" | "atmega16u4" | "atmega32a"
+        | "atmega32" | "atmega323" | "atmega324a" | "atmega324p" | "atmega324pa" | "atmega325"
+        | "atmega325a" | "atmega325p" | "atmega325pa" | "atmega3250" | "atmega3250a"
+        | "atmega3250p" | "atmega3250pa" | "atmega328" | "atmega328p" | "atmega328pb"
+        | "atmega329" | "atmega329a" | "atmega329p" | "atmega329pa" | "atmega3290"
+        | "atmega3290a" | "atmega3290p" | "atmega3290pa" | "atmega32c1" | "atmega32m1"
+        | "atmega32u4" | "atmega32u6" | "atmega406" | "atmega64" | "atmega64a" | "atmega640"
+        | "atmega644" | "atmega644a" | "atmega644p" | "atmega644pa" | "atmega645"
+        | "atmega645a" | "atmega645p" | "atmega6450" | "atmega6450a" | "atmega6450p"
+        | "atmega649" | "atmega649a" | "atmega649p" | "atmega6490" | "atmega16hva"
+        | "atmega16hva2" | "atmega32hvb" | "atmega6490a" | "atmega6490p" | "atmega64c1"
+        | "atmega64m1" | "atmega64hve" | "atmega64hve2" | "atmega64rfr2" | "atmega644rfr2"
+        | "atmega32hvbrevb" | "at90can32" | "at90can64" | "at90pwm161" | "at90pwm216"
+        | "at90pwm316" | "at90scr100" | "at90usb646" | "at90usb647" | "at94k" | "m3000" => Avr5,
 
-        "ATmega128" | "ATmega128A" | "ATmega1280" | "ATmega1281" | "ATmega1284" | "ATmega1284P"
-        | "ATmega128RFA1" | "ATmega128RFR2" | "ATmega1284RFR2" | "AT90CAN128" | "AT90USB1286"
-        | "AT90USB1287" => Avr51,
+        "atmega128" | "atmega128a" | "atmega1280" | "atmega1281" | "atmega1284" | "atmega1284p"
+        | "atmega128rfa1" | "atmega128rfr2" | "atmega1284rfr2" | "at90can128" | "at90usb1286"
+        | "at90usb1287" => Avr51,
 
-        "ATmega2560" | "ATmega2561" | "ATmega256RFR2" | "ATmega2564RFR2" => Avr6,
+        "atmega2560" | "atmega2561" | "atmega256rfr2" | "atmega2564rfr2" => Avr6,
 
-        "ATxmega8E5" | "ATxmega16A4" | "ATxmega16D4" | "ATxmega16E5" | "ATxmega32A4"
-        | "ATxmega32C3" | "ATxmega32D3" | "ATxmega32D4" | "ATxmega16A4U" | "ATxmega16C4"
-        | "ATxmega32A4U" | "ATxmega32C4" | "ATxmega32E5" => Xmega2,
+        "atxmega8e5" | "atxmega16a4" | "atxmega16d4" | "atxmega16e5" | "atxmega32a4"
+        | "atxmega32c3" | "atxmega32d3" | "atxmega32d4" | "atxmega16a4u" | "atxmega16c4"
+        | "atxmega32a4u" | "atxmega32c4" | "atxmega32e5" => Xmega2,
 
-        "ATtiny212" | "ATtiny214" | "ATtiny412" | "ATtiny414" | "ATtiny416" | "ATtiny417"
-        | "ATtiny814" | "ATtiny816" | "ATtiny817" | "ATtiny1614" | "ATtiny1616" | "ATtiny1617"
-        | "ATtiny3214" | "ATtiny3216" | "ATtiny3217" => Xmega3,
+        "attiny212" | "attiny214" | "attiny412" | "attiny414" | "attiny416" | "attiny417"
+        | "attiny814" | "attiny816" | "attiny817" | "attiny1614" | "attiny1616" | "attiny1617"
+        | "attiny3214" | "attiny3216" | "attiny3217" => Xmega3,
 
-        "ATxmega64A3" | "ATxmega64D3" | "ATxmega64A3U" | "ATxmega64A4U" | "ATxmega64B1"
-        | "ATxmega64B3" | "ATxmega64C3" | "ATxmega64D4" => Xmega4,
+        "atxmega64a3" | "atxmega64d3" | "atxmega64a3u" | "atxmega64a4u" | "atxmega64b1"
+        | "atxmega64b3" | "atxmega64c3" | "atxmega64d4" => Xmega4,
 
-        "ATxmega64A1" | "ATxmega64A1U" => Xmega5,
+        "atxmega64a1" | "atxmega64a1u" => Xmega5,
 
-        "ATxmega128A3" | "ATxmega128D3" | "ATxmega192A3" | "ATxmega192D3" | "ATxmega256A3"
-        | "ATxmega256A3B" | "ATxmega256A3BU" | "ATxmega256D3" | "ATxmega128A3U"
-        | "ATxmega128B1" | "ATxmega128B3" | "ATxmega128C3" | "ATxmega128D4" | "ATxmega192A3U"
-        | "ATxmega192C3" | "ATxmega256A3U" | "ATxmega256C3" | "ATxmega384C3" | "ATxmega384D3" => {
+        "atxmega128a3" | "atxmega128d3" | "atxmega192a3" | "atxmega192d3" | "atxmega256a3"
+        | "atxmega256a3b" | "atxmega256a3bu" | "atxmega256d3" | "atxmega128a3u"
+        | "atxmega128b1" | "atxmega128b3" | "atxmega128c3" | "atxmega128d4" | "atxmega192a3u"
+        | "atxmega192c3" | "atxmega256a3u" | "atxmega256c3" | "atxmega384c3" | "atxmega384d3" => {
             Xmega6
         }
 
-        "ATxmega128A1" | "ATxmega128A1U" | "ATxmega128A4U" => Xmega7,
+        "atxmega128a1" | "atxmega128a1u" | "atxmega128a4u" => Xmega7,
 
-        "ATtiny4" | "ATtiny5" | "ATtiny9" | "ATtiny10" | "ATtiny102" | "ATtiny20" | "ATtiny40" => {
+        "attiny4" | "attiny5" | "attiny9" | "attiny10" | "attiny102" | "attiny20" | "attiny40" => {
             Tiny
         }
 
-        "ATA8515" | "ATA5781" | "ATA5783" | "ATA5787" | "ATA5832" | "ATA5833" | "ATA5835"
-        | "ATmega324PB" | "ATtiny104" | "ATtiny80" | "ATtiny840" => Unknown,
+        "ata8515" | "ata5781" | "ata5783" | "ata5787" | "ata5832" | "ata5833" | "ata5835"
+        | "atmega324pb" | "attiny104" | "attiny80" | "attiny840" => Unknown,
 
         mcu_name => panic!("the AVR architecture name for MCU '{}' is unknown", mcu_name),
     }
 }
 
-fn c_preprocessor_name_from_mcu_name<'a>(mcu_name: &str) -> String {
-    format!("__AVR_{}__", mcu_name)
+fn c_preprocessor_name_from_mcu_name(mcu_name: &str) -> String {
+    let proper_mcu_name = mcu_name
+        .to_uppercase()
+        .replace("XMEGA", "xmega")
+        .replace("MEGA", "mega")
+        .replace("TINY", "tiny");
+    format!("__AVR_{}__", proper_mcu_name)
 }
 
 /// Looks up extra information about a microcontroller.
